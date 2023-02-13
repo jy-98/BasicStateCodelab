68b902dfb0cec049cd053049fb648cafd9955108
jiayang <1354856871@qq.com>
2023-02-13 09:46:17 +0800

    ADD：添加状态组合项

--- app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicstatecodelab.ui.WellnessScreen
+import com.example.basicstatecodelab.ui.WellnessTaskItem
 import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,17 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count: MutableState<Int> = remember { mutableStateOf(0) }
 
     Column(modifier = modifier.padding(16.dp)) {
+        var count by  rememberSaveable { mutableStateOf(0) }
 
-        Text(
-            text = "You've had ${count.value} glasses.",
-            modifier = modifier
-        )
-        Button(onClick = { count.value++ }, modifier.padding(top = 8.dp)) {
-            Text(text = "ADD one")
+        if (count > 0){
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask){
+                WellnessTaskItem("Have you taken your 15 minute walk today?",{showTask = false})
+            }
+            Text(text = "You've had $count glasses.",)
         }
+        Row( modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text(text = "ADD one")
+            }
+            Button(onClick = { count = 0 }, modifier.padding(start = 8.dp)) {
+                Text(text = "Clear water count")
+            }
+        }
+
     }
 }
 
